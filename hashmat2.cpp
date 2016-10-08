e9678caf6883bce146557c4092d5477acdff0a69
Ruby Anne S. Martin <ram199818@gmail.com>
2016-10-08 09:11:54 +0800

    Add files via upload

---
+++ hashmat2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    if stream f("sample.txt");
+    
+    int hashmat, opponent;
+    
+    while (f >> hashmat >> opponent) {
+          cout <<abs(hashmat+opponent);      
+    }
+    
+    //fclose(f);
+    system("pause");
+    return 0;
+}   
+
